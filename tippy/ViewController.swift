62659cc1172ffca0c426cd93f2c3619b5065c666
Kaila Prochaska <kailaprochaska@73.dhcp.apogeetelecom.com>
2018-08-26 10:48:33 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Kaila Prochaska on 8/26/18.
+//  Copyright © 2018 Kaila Prochaska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
